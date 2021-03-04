0baad8ac69a628a6e2f2ad8019cc974512d371ac
Andrew Webb <awebb@dataxstream.com>
2021-03-03 21:32:59 -0500

    Set up basic grid area for form content

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-
 import './App.css';
 
 function App() {
-
-
   return (
-    <div className="App">
-      
-    </div>
+    <main className="page-grid">
+      <div className="page-grid-sidebar" ></div>
+      <section className="page-grid-main" style={{background: "cornflowerblue"}}>
+        <div className="form-container"></div>
+          {/*//TODO: Contents Here*/}
+
+      </section>
+      <div className="page-grid-sidebar"></div>
+    </main>
   );
 }
 
