de5640f88a68aecda267d59ff7cf070b69fa2875
Andrew Webb <awebb@dataxstream.com>
2021-03-04 20:58:28 -0500

    Add formatting in/out for credit card value.

--- src/components/CardInput.js
+++ src/components/CardInput.js
@@ -1,10 +1,53 @@
 import React from 'react'
 import InputGroup from './InputGroup'
+import { removeSpaces, enforceCharLimit, enforceNoAlphaChars} from '../utilities/utilities'
 
 function CardInput({
   updateFormField,
   formState
 }) {
+
+  //TODO: Need function two functions. One to format incoming, and one for outgoing ( update state ).
+
+  const cardDisplayValue = (value) => {
+
+    let formattedString = ''
+    const spaceIndexes = [4, 8, 12]
+    
+    for (let i = 0; i < value.length; i++) {
+      if (spaceIndexes.includes(i)) {
+        formattedString += ' '
+      }
+
+      formattedString += value[i]
+    }
+
+    return formattedString
+  }
+
+  // const removeSpaces = (value) => value.replaceAll(' ', '')
+
+  // const enforceCharLimit = ({limit, value, previousValue}) => value.length <= limit ? value : previousValue
+
+  // const enforceNumericChars = ({value, previousValue}) => {
+  //   const hasLetters = /[a-zA-Z]/g.test(value);
+
+  //   return hasLetters ? previousValue : value
+  // }
+
+  const cleanTargetValue = (value) => {
+    const previousValue = formState.cardNumber
+    let newValue = value
+
+    newValue = enforceCharLimit({value:newValue, previousValue, limit: 19})
+    newValue = enforceNoAlphaChars({value:newValue, previousValue})
+    newValue = removeSpaces(newValue)
+
+    return newValue
+  }
+
+
+
   return (
     <InputGroup 
       label="Card Number"
@@ -12,8 +55,8 @@ function CardInput({
       type="text"
       addedGroupClasses={['creditCard']}
       updateFormField={updateFormField}
-      onChange={(e) => updateFormField('cardNumber', e.target.value)}
-      value={formState.cardNumber}
+      onChange={(e) => updateFormField('cardNumber', cleanTargetValue(e.target.value))}
+      value={cardDisplayValue(formState.cardNumber)}
        />
   )
 }
